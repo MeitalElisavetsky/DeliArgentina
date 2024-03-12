fcee8e739676e6976d497d6e6cbb00299a9c65e1
MeitalElisavetsky <meitalelisavetsky@hotmail.com>
2024-03-12 11:15:26 +0200

    Changed mongo's URI

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 from bson import ObjectId
 
 app = Flask(__name__, static_url_path='/static')
-app.config['MONGO_URI'] = 'mongodb://mongodb:27017/deli_argentina_db'
+app.config['MONGO_URI'] = "mongodb://mongodb.default.svc.cluster.local:27017/deli_argentina_db"
 mongo = PyMongo(app)
 
 def initialize_database():
